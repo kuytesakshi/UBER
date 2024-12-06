6488235173296cfd1bc55526c96934a1516ed063
Sakshi Kuyte <kuytesakshi@gmail.com>
2024-12-06 23:06:01 +0530

    user authorization done.........

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
+dotenv.config()
 const cors = require('cors')
-
-
 const app = express()
+const connectToDb =require('./db/db')
+const userRoutes = require('./routes/user.route')
+const cookieParser = require('cookie-parser')
 
+
+connectToDb()
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
 app.get('/',(req,res)=>{
     res.send('hello world...')
 })
+app.use('/users', userRoutes)
+
 module.exports = app
